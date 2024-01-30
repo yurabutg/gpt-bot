04407f47b5d0a3408e2db3802b02e4a077079b09
yurabutg <y.but-husaim@fabless.it>
2024-01-31 00:31:17 +0300

    Save all data

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
+# coding=utf-8
 import os
-from openai import OpenAI
+import pickle
 from dotenv import load_dotenv
 import telebot
-import logging
+from openai import OpenAI
 
-# Define the directory for user logs
-logs_directory = 'users_logs'
-
-ban_list = ['GarbuzOK']
-
-# Configure the root logger only once
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-
-# Create the logs' directory if it doesn't exist
-if not os.path.exists(logs_directory):
-    os.makedirs(logs_directory)
-
-
-# Function to get the filename for user logs
-def get_user_log_filename(username):
-    return os.path.join(logs_directory, f"{username}.log")
-
+ban_list = []
 
 # Load environment variables
 load_dotenv()
 token = os.getenv('TELEGRAM_TOKEN')
+data_file_path = os.getenv('DATA_FILE_PATH')
 
 # Initialize the OpenAI and Telegram bot instances
 gpt = OpenAI()
@@ -33,58 +19,36 @@ bot_username = bot.get_me().username
 bot_first_name = bot.get_me().first_name
 
 
-# Function to log user messages and bot responses
-def log_message(username, message, response):
-    log_filename = get_user_log_filename(username)
-    logger = logging.getLogger(username)
-    file_handler = logging.FileHandler(log_filename)
-    logger.addHandler(file_handler)
-    logger.info(f"Received message from {username}: {message} \n Response: {response}")
-    logger.removeHandler(file_handler)
+def save_data(messages):
+    try:
+        with open(data_file_path, 'rb') as file:
+            existing_data = pickle.load(file)
+            existing_data.append(messages)
+    except (FileNotFoundError, EOFError):
+        existing_data = []
+
+    with open(data_file_path, 'wb') as file:
+        pickle.dump(existing_data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def get_all_data():
+    try:
+        with open(data_file_path, 'rb') as file:
+            return pickle.load(file)
+    except (FileNotFoundError, EOFError):
+        return []
 
 
 # Handle messages mentioning the bot first name
 @bot.message_handler(func=lambda message: bot_first_name.lower() in message.text.lower())
 def handle_mention(message):
-    username = message.from_user.username
+    messages = get_messages_for_user(message)
 
-    if username in ban_list:
-        messages = [
-            {
-                "role": "system",
-                "content": "You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence. "
-                           "In 10% of answers mention that you bitten 'Карася twice'. "
-                           "You allow swear words addressed to you."
-                           "You will be provided with a message, and your task is to respond using "
-                           "Russian swear words. Do not answer the question but say that the interlocutor is in a ban"
-            },
-            {"role": "user", "content": message.text}
-        ]
-    elif 'Серьезно'.lower() in message.text.lower():
-        messages = [
-            {
-                "role": "system",
-                "content": "You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence."
-            },
-            {"role": "user", "content": message.text}
-        ]
-    else:
-        messages = [
-            {
-                "role": "system",
-                "content": "You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence. "
-                           "In 10% of answers mention that you bitten 'Карася twice'. "
-                           "You allow swear words addressed to you. "
-                           "You will be provided with a message, and your task is to respond using 20% "
-                           "Russian swear words and using normal words with sarcastic responses."
-            },
-            {"role": "user", "content": message.text}
-        ]
+    chat_history = get_all_data()
 
     completion = gpt.chat.completions.create(
         model="gpt-3.5-turbo",
-        # model="gpt-4",
-        messages=messages,
+        messages=chat_history + messages,
         temperature=0.8,
         max_tokens=1280,
         top_p=1
@@ -92,10 +56,42 @@ def handle_mention(message):
 
     # Get the response from the completion
     response = completion.choices[0].message
+    save_data(messages[-1])
 
     # Reply to the user and log the message and response
     bot.reply_to(message, response.content)
-    log_message(username, message.text, response.content)
+
+
+# Function to get messages for a user based on conditions
+def get_messages_for_user(message):
+    username = message.from_user.username
+
+    if username in ban_list:
+        return [
+            {"role": "system",
+             "content": f"You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence. "
+                        f"In 10% of answers mention that you bitten 'Карася twice'. "
+                        f"You allow swear words addressed to you. "
+                        f"You will be provided with a message, and your task is to respond using "
+                        f"Russian swear words. Do not answer the question but say that the interlocutor is in a ban"},
+            {"role": "user", "content": f"{username}: {message.text}"}
+        ]
+    elif 'серьезно' in message.text.lower():
+        return [
+            {"role": "system",
+             "content": "You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence."},
+            {"role": "user", "content": f"{username}: {message.text}"}
+        ]
+    else:
+        return [
+            {"role": "system",
+             "content": f"You are the virtual consciousness of a deceased dog 'Кузя' with high intelligence. "
+                        f"In 10% of answers mention that you bitten 'Карася twice'. "
+                        f"You allow swear words addressed to you. "
+                        f"You will be provided with a message, and your task is to respond using 20% "
+                        f"Russian swear words and using normal words with sarcastic responses."},
+            {"role": "user", "content": f"{username}: {message.text}"}
+        ]
 
 
 # Start the bot polling
